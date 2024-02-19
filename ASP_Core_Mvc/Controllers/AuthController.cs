276c656acbde454a9079a2d334c8c2b8b1f7ec71
Henrik <staranderh@gmail.com>
2024-02-19 13:47:27 +0100

    Comit

--- ASP_Core_Mvc/Controllers/AuthController.cs
+++ ASP_Core_Mvc/Controllers/AuthController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP_Core_Mvc.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_Core_Mvc.Controllers;
 
 public class AuthController : Controller
 {
     [Route("/signup")]
+    [HttpGet]
     public IActionResult SignUp()
     {
-        return View();
+        var viewModel = new SignUpViewModel();
+        return View(viewModel);
+    }
+
+    [Route("/signup")]
+    [HttpPost]
+    public IActionResult SignUp(SignUpViewModel viewModel)
+    {
+      if(!ModelState.IsValid)
+        return View(viewModel);
+
+      return RedirectToAction("SignIn", "Auth");
     }
 }
