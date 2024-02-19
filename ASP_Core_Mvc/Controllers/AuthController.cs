55ddc616e961c306daa55f0d537d5464e21d7381
Henrik <staranderh@gmail.com>
2024-02-19 11:33:29 +0100

    fixat struktur

---
+++ ASP_Core_Mvc/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Core_Mvc.Controllers;
+
+public class AuthController : Controller
+{
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+}
