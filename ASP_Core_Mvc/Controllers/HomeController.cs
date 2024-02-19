1287962e5db417c35211ac39d2dce16385ef17d6
Henrik <staranderh@gmail.com>
2024-02-19 09:59:32 +0100

    Add project files.

---
+++ ASP_Core_Mvc/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Core_Mvc.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        //ViewData["Title"] = "Ultimate Task management Asisstant you gonna Love";
+        return View();
+    }
+}
