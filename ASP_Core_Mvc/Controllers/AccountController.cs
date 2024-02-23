b5c9acf2d2d50bdfce5ae14204521bde240e0221
Henrik <staranderh@gmail.com>
2024-02-23 15:56:14 +0100

    accoount detail wokring

--- ASP_Core_Mvc/Controllers/AccountController.cs
+++ ASP_Core_Mvc/Controllers/AccountController.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP_Core_Mvc.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_Core_Mvc.Controllers;
 
 public class AccountController : Controller
 {
-  
+    [Route("/details")]
+    public IActionResult Details()
+    {
+        var viewModel = new AccountDetailsViewModel();
+        return View(viewModel);
+    }
 }
---
+++ ASP_Core_Mvc/ViewModels/AccountDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ASP_Core_Mvc.Models;
+
+namespace ASP_Core_Mvc.ViewModels;
+
+public class AccountDetailsViewModel
+{
+    public string Title { get; set; } = "Account Details";
+
+    public AccountDetailsBasicInfoModel BasicInfo { get; set; } = new AccountDetailsBasicInfoModel()
+    {
+        ProfileImage = "/images/avatar.svg",
+        FirstName = "Henrik",
+        LastName = "Starander",
+        EmailAdress = "Henrik@domain.com"
+
+    };
+
+    public AccountDetailAdressInfoModel AdressInfoModel { get; set; } = new AccountDetailAdressInfoModel();
+
+
+}
