56d773bcc5bfc17b713a5f7e9a8c696d2514ccc3
Henrik <staranderh@gmail.com>
2024-02-26 12:03:17 +0100

    Klar med css html

---
+++ ASP_Core_Mvc/Controllers/PageNotFoundController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Core_Mvc.Controllers
+{
+    public class PageNotFoundController : Controller
+    {
+        [Route("/pagenotfound")]
+        public IActionResult PageNotFound()
+        {
+            return View();
+        }
+    }
+}
